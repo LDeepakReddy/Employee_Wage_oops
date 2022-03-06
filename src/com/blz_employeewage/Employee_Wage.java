9cc333c463e77c2f520a93524eb9787260d6f6b8
LDeepakReddy <l.deepakreddy.com>
2022-03-07 02:13:16 +0530

    Added:feature to check employee is present or absent

--- src/com/blz_employeewage/Employee_Wage.java
+++ src/com/blz_employeewage/Employee_Wage.java
@@ -4,5 +4,16 @@ public class Employee_Wage {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Employee wage program");
+        int Present = 1;
+        int attendance = (int) (Math.random() * 10) % 2;
+
+        if (attendance == Present) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
+
+
+
