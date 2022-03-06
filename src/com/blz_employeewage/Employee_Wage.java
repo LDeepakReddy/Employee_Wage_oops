928fedb889a8bb1960a16b2b709fb87b84043e86
LDeepakReddy <l.deepakreddy.com>
2022-03-07 03:39:06 +0530

    Added:Monthly employee wage for multiple companies

--- src/com/blz_employeewage/Employee_Wage.java
+++ src/com/blz_employeewage/Employee_Wage.java
@@ -3,11 +3,9 @@ package com.blz_employeewage;
 public class Employee_Wage {
     public static final int FULL_TIME = 1;
     public static final int PART_TIME = 2;
-    public static final int EMP_WAGE_PER_HR = 20;
-    public static final int MAX_WORKING_DAYS = 20;
-    public static final int MAX_WORKING_HRS = 100;
 
-    public static void computeWage() {
+
+    public static void computeWage(String company, int EMP_WAGE_PER_HR, int MAX_WORKING_DAYS, int MAX_WORKING_HRS) {
         int empHr = 0;
         int totalWorkingDays = 0;
         int totalEmpHrs = 0;
@@ -35,12 +33,14 @@ public class Employee_Wage {
             }
             totalEmpWage = totalEmpHrs * EMP_WAGE_PER_HR;
             System.out.println("Employee worked for " + totalWorkingDays + " days " + totalEmpHrs + " hours");
-            System.out.println("Employee Monthly wage is " + totalEmpWage);
+            System.out.println("Employee Monthly wage is " + totalEmpWage + " In " + company);
         }
     }
 
     public static void main(String[] args) {
-        computeWage();
+        computeWage("AZtek", 45, 20, 100);
+        computeWage("Agglotek", 30, 25, 100);
+        computeWage("Arteras", 20, 20, 80);
     }
 }
 
