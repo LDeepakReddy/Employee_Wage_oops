b778ead3dd9101c092fc2aa2717af538cf89c5ad
LDeepakReddy <l.deepakreddy.com>
2022-03-07 02:20:17 +0530

    added:Daily employeewage calculation

--- src/com/blz_employeewage/Employee_Wage.java
+++ src/com/blz_employeewage/Employee_Wage.java
@@ -1,17 +1,24 @@
 package com.blz_employeewage;
 
 public class Employee_Wage {
+    public static final int FULL_TIME = 1;
+    public static final int EMP_WAGE_PER_HR = 20;
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Employee wage program");
+        int empHr = 0;
+        int empWage;
+
         int Present = 1;
         int attendance = (int) (Math.random() * 10) % 2;
-
-        if (attendance == Present) {
+        if (attendance == FULL_TIME) {
+            empHr = 8;
             System.out.println("Employee is present");
         } else {
             System.out.println("Employee is Absent");
         }
+        empWage = empHr * EMP_WAGE_PER_HR;
+        System.out.println("Employee's wage is " + empWage);
+
     }
 }
 
