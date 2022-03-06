70d612341d184a3453b799419484967ed94eb6a7
LDeepakReddy <l.deepakreddy.com>
2022-03-07 02:25:33 +0530

    Added:Parttime wage of employee

--- src/com/blz_employeewage/Employee_Wage.java
+++ src/com/blz_employeewage/Employee_Wage.java
@@ -2,6 +2,7 @@ package com.blz_employeewage;
 
 public class Employee_Wage {
     public static final int FULL_TIME = 1;
+    public static final int PART_TIME = 2;
     public static final int EMP_WAGE_PER_HR = 20;
 
     public static void main(String[] args) {
@@ -9,15 +10,18 @@ public class Employee_Wage {
         int empWage;
 
         int Present = 1;
-        int attendance = (int) (Math.random() * 10) % 2;
+        int attendance = (int) (Math.random() * 10) % 3;
         if (attendance == FULL_TIME) {
             empHr = 8;
             System.out.println("Employee is present");
+        } else if (attendance == PART_TIME) {
+            empHr = 4;
+            System.out.println("Employee is working Part Time");
         } else {
             System.out.println("Employee is Absent");
         }
         empWage = empHr * EMP_WAGE_PER_HR;
-        System.out.println("Employee's wage is " + empWage);
+        System.out.println("Employee wage is " + empWage);
 
     }
 }
