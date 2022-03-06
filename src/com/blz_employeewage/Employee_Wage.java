a40932ab44488749a74aab516370cccfcd78454f
LDeepakReddy <l.deepakreddy.com>
2022-03-07 02:06:58 +0530

    Added:Welcome message

---
+++ src/com/blz_employeewage/Employee_Wage.java
@@ -0,0 +1,8 @@
+package com.blz_employeewage;
+
+public class Employee_Wage {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Employee wage program");
+    }
+}
